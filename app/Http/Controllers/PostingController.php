fe5235c097e025ef7c218fc6d1819a0440e3c2e0
ardasatata <ardasatata@gmail.com>
2017-11-14 17:26:21 +0700

    edit posting controller

--- app/Http/Controllers/PostingController.php
+++ app/Http/Controllers/PostingController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Posting;
 
 class PostingController extends Controller
 {
-    //
+    public function createPost(Request $request){
+
+    	$id=Auth::id();
+
+    }
 }

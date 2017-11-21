49b4d87107426dced12f634d21b180070192ccda
Arda-PC <ardasatata@gmail.com>
2017-11-22 00:24:40 +0700

    update posting

--- resources/views/posting/create.blade.php
+++ resources/views/posting/create.blade.php
@@ -8,13 +8,19 @@
                     <div class="panel-heading">Dashboard</div>
 
                     <div class="panel-body">
+
                         <form action = "/post/posting" method = "post">
                             <input type = "hidden" name = "_token" value = "<?php echo csrf_token(); ?>">
 
                             <table>
                                 <tr>
-                                    <td>Name</td>
-                                    <td><input type='text' name='stud_name' /></td>
+                                    <td>Judul Posting</td>
+                                    <td><input type='text' name='Judul Post' /></td>
+                                </tr>
+
+                                <tr>
+                                    <td>Caption</td>
+                                    <td><input type='text' name='Caption' /></td>
                                 </tr>
                                 <tr>
                                     <td colspan = '2'>
@@ -24,6 +30,7 @@
                             </table>
 
                         </form>
+
                     </div>
                 </div>
             </div>
